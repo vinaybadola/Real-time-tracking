13384cae58060f3032953a8812d6ce7fa074a50e
vinaybadola <vinaybadola.cs18@gmail.com>
2024-07-19 03:06:24 +0530

    add the location finder of the user

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const socketio = require('socket.io');
 const http = require('http');
 const path = require('path');
@@ -8,11 +7,24 @@ const path = require('path');
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req,res) =>{
-    res.send('hello world');
+io.on("connection", (socket) => {
+    console.log('A user connected: ' + socket.id);
+
+    socket.on('send-Location', (data) => {
+        io.emit('receive-location', { id: socket.id, ...data });
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('remove-location', socket.id);
+        console.log('A user disconnected: ' + socket.id);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 server.listen(3000, () => {
---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+const socket = io();
+
+// Check if the browser supports geolocation 
+if (!navigator.geolocation) {
+    alert('Geolocation is not supported by your browser');
+} else {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit('send-Location', { latitude, longitude });
+        }, 
+        (error) => {
+            console.log(error);
+        },  
+        {
+            enableHighAccuracy: true, // true means more accurate but may take longer
+            timeout: 5000, 
+            maximumAge: 0  // 0 means no cache
+        }
+    );
+}
+
+const map = L.map("map").setView([0, 0], 16);  // 0,0 is the lat and long, 16 is the zoom level of the map 
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const markers = {};
+
+socket.on('receive-location', (data) => {
+    const { id, latitude, longitude } = data;
+    console.log(`Received location for ${id}: ${latitude}, ${longitude}`);
+    map.setView([latitude, longitude]);  // set the view of the map to the new location
+
+    if (markers[id]) {
+        // markers[id].setLatLng([latitude, longitude]);  // update the marker position
+        map.removeLayer(markers[id]);
+    }
+
+    markers[id] = L.marker([latitude, longitude]).addTo(map);  // add the new marker to the map
+});
+
+socket.on('remove-location', (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);  // remove the marker from the map
+        delete markers[id];  // remove the marker from the markers object
+    }
+});
